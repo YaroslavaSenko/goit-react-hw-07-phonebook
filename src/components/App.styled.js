bef251f2898de5d6cb0c498790fce49349268411
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-18 22:59:52 +0300

    Create App.styled.js

---
+++ src/components/App.styled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Contacts = styled.div`
+width: 370px;
+margin-left: auto;
+margin-right: auto;
+margin-top: 50px;
+padding: 40px;
+background-color: white;
+border: 1px solid rgb(216, 228, 226);
+border-radius: 8px;
+overflow: hidden;
+box-shadow: rgb(0 0 0 / 20%) 0px 3px 3px -2px, rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px;`
