45cb9dc25cfdf593af4ac03ba647a4f32438da98
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-25 19:30:19 +0300

    Create Filter.styled.js

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const FormField = styled.label`
+display: block;
+margin-top: 20px;
+margin-bottom: 20px;
+`;
+
+export const Input = styled.input`
+height: auto;
+padding: 10px;
+background-color: white;
+border: 1px solid #d8e4e2;
+border-radius: 8px;
+`;
