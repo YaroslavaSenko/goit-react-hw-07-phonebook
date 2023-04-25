42f40563d9dfc77deed627f4cf744e2842ce8ba9
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-25 19:28:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,26 @@ import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 // import { nanoid } from 'nanoid';
 import { Contacts } from './App.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import { getError, getIsLoading } from 'redux/selectors';
 
 export const App = () => {
+
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError)
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <Contacts>
-      <h1>Phonebook</h1>
-    
-
+    <Contacts>      
       <ContactForm />
-
-      <h2>Contacts</h2>
+      {isLoading && !error && <b>Request in progress...</b>}
       <Filter />
-
       <ContactList />
     </Contacts>
   );
